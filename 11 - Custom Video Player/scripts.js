4b90f92eb14edfe9edcbd4a1c3e0d78077484c5a
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-07-19 13:29:40 -0400

    Add skip function
    Function skips ahead or backwards to reflect the demanded quantity.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -19,9 +19,13 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
-
 toggle.addEventListener('click', togglePlay)
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
